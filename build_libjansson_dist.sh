0defec62c43d92c56ff5b72dadf08013e2656f94
matsurowa25 <34836335+matsurowa25@users.noreply.github.com>
2018-01-05 00:24:29 +0900

    Update build_libjansson_dist.sh

--- build_libjansson_dist.sh
+++ build_libjansson_dist.sh
@@ -11,28 +11,29 @@ function build_for_arch() {
   PREFIX=$4
   IPHONEOS_DEPLOYMENT_TARGET="9.0"
   export PATH="${DEVROOT}/usr/bin/:${PATH}"
-  export CC="/usr/bin/clang"
-  export CPP="clang -E"
-  #export CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
-  export CFLAGS="-arch ${ARCH} -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET}"
+  #export CC="/usr/bin/clang"
+  #export CPP="clang -E"
+  export CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
+  #export CFLAGS="-arch ${ARCH} -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET}"
   export LDFLAGS="-arch ${ARCH} -isysroot ${SYSROOT}"
   #./configure --disable-shared --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
   #./configure --disable-shared --host="${HOST}" --prefix=${PREFIX} && make -j8
-  ./configure --host="${HOST}" --prefix=${PREFIX} && make
+  make clean
+  ./configure --disable-shared --host="${HOST}" --prefix=${PREFIX} && make
 }
 
 TMP_DIR=/tmp/build_libjansson_$$
 
 #build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
-#build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
+build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
 build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/arm64 || exit 3
-#build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
+build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
 #build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7 || exit 5
 
 mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
-#	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
-#	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
+	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
+	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
 	-arch arm64 ${TMP_DIR}/arm64/lib/libcurl.a \
 	-output ${TMP_DIR}/lib/libcurl.a -create
 
