71cf99142b61f547ba38257e9274424b6fee935f
matsurowa25 <34836335+matsurowa25@users.noreply.github.com>
2018-01-05 00:08:50 +0900

    Update build_libjansson_dist.sh

--- build_libjansson_dist.sh
+++ build_libjansson_dist.sh
@@ -19,15 +19,15 @@ function build_for_arch() {
 TMP_DIR=/tmp/build_libjansson_$$
 
 #build_for_arch i386 i386-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/i386 || exit 1
-build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
+#build_for_arch x86_64 x86_64-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk ${TMP_DIR}/x86_64 || exit 2
 build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/arm64 || exit 3
-build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
+#build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7s || exit 4
 #build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk ${TMP_DIR}/armv7 || exit 5
 
 mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
-	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
-	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
+#	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
+#	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
 	-arch arm64 ${TMP_DIR}/arm64/lib/libcurl.a \
 	-output ${TMP_DIR}/lib/libcurl.a -create
 
